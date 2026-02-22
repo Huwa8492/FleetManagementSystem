aa7b00bda24dab8647faea5f4c65ea8ba0135f55
Huwa8492 <huwacoding@gmail.com>
2026-02-22 16:07:46 +0100

    Created Tables

---
+++ schema.sql
@@ -0,0 +1,38 @@
+-- Deletes the tables if they already exists
+DROP TABLE IF EXISTS MaINTEGERenance;
+DROP TABLE IF EXISTS Trips;
+DROP TABLE IF EXISTS Vehicles;
+DROP TABLE IF EXISTS Drivers;
+
+CREATE TABLE Drivers (
+    DriverID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name VARCHAR(50) NOT NULL,
+    LicenseType VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Vehicles (
+    VehicleID INTEGER PRIMARY KEY AUTOINCREMENT,
+    LicensePlate VARCHAR(50) UNIQUE NOT NULL,
+    Mileage INTEGER DEFAULT (0),
+    Status VARCHAR(50) DEFAULT 'Active'
+);
+
+CREATE TABLE MaINTEGERenance (
+    MaINTEGERenanceID INTEGER PRIMARY KEY AUTOINCREMENT,
+    VehicleID INTEGER NOT NULL,
+    Date DATE NOT NULL,
+    Cost REAL,
+    Description VARCHAR(255) NOT NULL,
+    FOREIGN KEY (VehicleID) REFERENCES Vehicles(VehicleID)
+);
+
+CREATE TABLE Trips (
+    TripID INTEGER PRIMARY KEY AUTOINCREMENT,
+    VehicleID INTEGER NOT NULL,
+    DriverID INTEGER NOT NULL,
+    Date DATE NOT NULL,
+    Distance INTEGER NOT NULL,
+    Cost REAL,
+    FOREIGN KEY (VehicleID) REFERENCES Vehicles(VehicleID),
+    FOREIGN KEY (DriverID) REFERENCES Drivers(DriverID)
+);
