eedc40b86f036144c20842b588cb9970121ddba0
Huwa8492 <huwacoding@gmail.com>
2026-03-13 20:34:18 +0100

    Database dump

--- database_dump.sql
+++ database_dump.sql
@@ -1,72 +1,210 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-CREATE TABLE Drivers (
-    DriverID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Name VARCHAR(50) NOT NULL,
-    LicenseType VARCHAR(50) NOT NULL
-);
-INSERT INTO Drivers VALUES(1,'John Johansson','B');
-INSERT INTO Drivers VALUES(2,'Anna Anderson','C');
-INSERT INTO Drivers VALUES(3,'Jacob Karlsson','B');
-INSERT INTO Drivers VALUES(4,'Erik Svensson','CE');
-INSERT INTO Drivers VALUES(5,'Axel Lindgren','C');
-CREATE TABLE Vehicles (
-    VehicleID INTEGER PRIMARY KEY AUTOINCREMENT,
-    LicensePlate VARCHAR(50) UNIQUE NOT NULL,
-    Mileage INTEGER DEFAULT (0),
-    Status VARCHAR(50) DEFAULT 'Active'
-);
-INSERT INTO Vehicles VALUES(1,'QWE-123',70123,'Active');
-INSERT INTO Vehicles VALUES(2,'RTY-456',4400,'Active');
-INSERT INTO Vehicles VALUES(3,'UIO-789',12000,'Maintenance Required');
-INSERT INTO Vehicles VALUES(4,'AMH-285',700,'Active');
-INSERT INTO Vehicles VALUES(5,'LMO-916',35000,'Active');
-CREATE TABLE Maintenance (
-    MaintenanceID INTEGER PRIMARY KEY AUTOINCREMENT,
-    VehicleID INTEGER NOT NULL,
-    Date DATE NOT NULL,
-    Cost REAL,
-    Description VARCHAR(255) NOT NULL,
-    FOREIGN KEY (VehicleID) REFERENCES Vehicles(VehicleID)
-);
-INSERT INTO Maintenance VALUES(1,2,'2026-03-13',20000.0,'Oil change');
-INSERT INTO Maintenance VALUES(2,1,'2026-03-12',10000.0,'Service');
-CREATE TABLE Trips (
-    TripID INTEGER PRIMARY KEY AUTOINCREMENT,
-    VehicleID INTEGER NOT NULL,
-    DriverID INTEGER NOT NULL,
-    Date DATE NOT NULL,
-    Distance INTEGER NOT NULL,
-    Cost REAL,
-    FOREIGN KEY (VehicleID) REFERENCES Vehicles(VehicleID),
-    FOREIGN KEY (DriverID) REFERENCES Drivers(DriverID)
-);
-INSERT INTO Trips VALUES(1,1,2,'2026-03-11',123,2000.0);
-INSERT INTO Trips VALUES(2,1,2,'2026-03-06',1000,20000.0);
-INSERT INTO Trips VALUES(3,1,1,'2026-05-23',5000,15000.0);
-DELETE FROM sqlite_sequence;
-INSERT INTO sqlite_sequence VALUES('Drivers',5);
-INSERT INTO sqlite_sequence VALUES('Vehicles',5);
-INSERT INTO sqlite_sequence VALUES('Trips',3);
-INSERT INTO sqlite_sequence VALUES('Maintenance',2);
-CREATE TRIGGER Check_Maintenance_Trigger
-AFTER INSERT ON Trips
-BEGIN
+-- MySQL dump 10.13  Distrib 8.0.31, for macos12 (arm64)
+--
+-- Host: localhost    Database: FleetManagement
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Drivers`
+--
+
+DROP TABLE IF EXISTS `Drivers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Drivers` (
+  `DriverID` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
+  `LicenseType` varchar(50) NOT NULL,
+  PRIMARY KEY (`DriverID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Drivers`
+--
+
+LOCK TABLES `Drivers` WRITE;
+/*!40000 ALTER TABLE `Drivers` DISABLE KEYS */;
+INSERT INTO `Drivers` VALUES (1,'John Johansson','B'),(2,'Anna Anderson','C'),(3,'Jacob Karlsson','B'),(4,'Erik Svensson','CE'),(5,'Axel Lindgren','C');
+/*!40000 ALTER TABLE `Drivers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Maintenance`
+--
+
+DROP TABLE IF EXISTS `Maintenance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Maintenance` (
+  `MaintenanceID` int NOT NULL AUTO_INCREMENT,
+  `VehicleID` int NOT NULL,
+  `Date` date NOT NULL,
+  `Cost` decimal(10,2) DEFAULT NULL,
+  `Description` varchar(255) NOT NULL,
+  PRIMARY KEY (`MaintenanceID`),
+  KEY `VehicleID` (`VehicleID`),
+  CONSTRAINT `maintenance_ibfk_1` FOREIGN KEY (`VehicleID`) REFERENCES `Vehicles` (`VehicleID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Maintenance`
+--
+
+LOCK TABLES `Maintenance` WRITE;
+/*!40000 ALTER TABLE `Maintenance` DISABLE KEYS */;
+INSERT INTO `Maintenance` VALUES (1,1,'2026-03-19',12000.00,'Oil change and new brakes');
+/*!40000 ALTER TABLE `Maintenance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Trips`
+--
+
+DROP TABLE IF EXISTS `Trips`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Trips` (
+  `TripID` int NOT NULL AUTO_INCREMENT,
+  `VehicleID` int NOT NULL,
+  `DriverID` int NOT NULL,
+  `Date` date NOT NULL,
+  `Distance` int NOT NULL,
+  `Cost` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`TripID`),
+  KEY `VehicleID` (`VehicleID`),
+  KEY `DriverID` (`DriverID`),
+  CONSTRAINT `trips_ibfk_1` FOREIGN KEY (`VehicleID`) REFERENCES `Vehicles` (`VehicleID`),
+  CONSTRAINT `trips_ibfk_2` FOREIGN KEY (`DriverID`) REFERENCES `Drivers` (`DriverID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Trips`
+--
+
+LOCK TABLES `Trips` WRITE;
+/*!40000 ALTER TABLE `Trips` DISABLE KEYS */;
+INSERT INTO `Trips` VALUES (1,1,3,'2026-03-14',8900,4500.00),(2,4,4,'2026-03-14',460,800.00);
+/*!40000 ALTER TABLE `Trips` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `Check_Maintenance_Trigger` AFTER INSERT ON `trips` FOR EACH ROW BEGIN
     UPDATE Vehicles
     SET
         Status = CASE
-                WHEN (Mileage + NEW.Distance)/10000 > Mileage/10000 THEN 'Maintenance Required'
+                WHEN (Mileage + NEW.Distance) DIV 10000 > Mileage DIV 10000 THEN 'Maintenance Required'
                 ELSE Status
             END,
         Mileage = Mileage + NEW.Distance
     WHERE VehicleID = NEW.VehicleID;
-END;
-CREATE TRIGGER Prevent_Active_Vehicle_Deletion
-BEFORE DELETE ON Vehicles
-BEGIN 
-    SELECT CASE
-        WHEN OLD.Status = 'Active' THEN
-            RAISE(ABORT, 'Cannot delete a vehicle that is currently active.')
-    END;
-END;
-COMMIT;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `Vehicles`
+--
+
+DROP TABLE IF EXISTS `Vehicles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Vehicles` (
+  `VehicleID` int NOT NULL AUTO_INCREMENT,
+  `LicensePlate` varchar(50) NOT NULL,
+  `Mileage` int DEFAULT '0',
+  `Status` varchar(50) DEFAULT 'Active',
+  PRIMARY KEY (`VehicleID`),
+  UNIQUE KEY `LicensePlate` (`LicensePlate`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Vehicles`
+--
+
+LOCK TABLES `Vehicles` WRITE;
+/*!40000 ALTER TABLE `Vehicles` DISABLE KEYS */;
+INSERT INTO `Vehicles` VALUES (1,'QWE-123',72900,'Active'),(2,'RTY-456',4400,'Active'),(3,'UIO-789',12000,'Maintenance Required'),(4,'AMH-285',1160,'Active'),(5,'LMO-916',35000,'Active');
+/*!40000 ALTER TABLE `Vehicles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `Prevent_Active_Vehicle_Deletion` BEFORE DELETE ON `vehicles` FOR EACH ROW BEGIN 
+    IF OLD.Status = 'Active' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Cannot delete a vehicle that is currently active.';
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Dumping routines for database 'FleetManagement'
+--
+/*!50003 DROP FUNCTION IF EXISTS `CALCULATE_TAX` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `CALCULATE_TAX`(trip_cost DECIMAL(10,2)) RETURNS decimal(10,2)
+    DETERMINISTIC
+BEGIN
+    IF trip_cost IS NULL THEN RETURN 0.00;
+    END IF;
+    RETURN ROUND(trip_cost * 0.25, 2);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-13 20:33:14
